967984eb701c960e262e5ac0ea090a92ad1388dc
UzairGhaffar1144 <uzairghaffar1144@gmail.com>
2023-03-28 00:44:51 +0500

    d

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ var config = require("config");
 var cors = require("cors");
 var app = express();
 app.use(cors());
-
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
