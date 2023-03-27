b986a64cfd5717d6ecda2e15728247c9a1dafc69
UzairGhaffar1144 <uzairghaffar1144@gmail.com>
2023-03-28 00:40:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var config = require("config");
 var cors = require("cors");
 var app = express();
 app.use(cors());
-s;
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
