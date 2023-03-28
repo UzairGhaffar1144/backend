7a89d8f78cd2167701cae6e80fedb5a8b4f9610f
UzairGhaffar1144 <uzairghaffar1144@gmail.com>
2023-03-28 20:45:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(cors());
 // view engine setup
 // app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "jade");
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
