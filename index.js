f8d5e1e7448b2a0d7626874af8a1302195386dfa
UzairGhaffar1144 <uzairghaffar1144@gmail.com>
2023-03-28 20:53:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  res.send(html);
+  res.send(err);
   // // render the error page
   // res.status(err.status || 500);
   // res.render("error");
