29becc1d22421173be3170edd34d1725979cb7be
UzairGhaffar1144 <uzairghaffar1144@gmail.com>
2023-03-28 21:14:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ var cors = require("cors");
 var app = express();
 app.use(cors());
 // view engine setup
+
+//view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
