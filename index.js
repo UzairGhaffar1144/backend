8b5ef2c9f8cbc83afdaea4ac3e90201b06c5a957
UzairGhaffar1144 <uzairghaffar1144@gmail.com>
2023-03-28 20:50:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -49,9 +49,10 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.send(html);
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.render("error");
 });
 mongoose
   .connect(config.get("db"), {
