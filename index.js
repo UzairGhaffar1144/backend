a783889c147ff381dba8084ac9d4f5aea0be4a47
UzairGhaffar1144 <uzairghaffar1144@gmail.com>
2023-03-28 21:23:31 +0500

    view changed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors());
 //view engine
 
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
