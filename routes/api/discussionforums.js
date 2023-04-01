132b80da1bb7caa61fb1463e3528a8e50818cb6e
UzairGhaffar1144 <uzairghaffar1144@gmail.com>
2023-04-01 22:40:05 +0500

    Update discussionforums.js

--- routes/api/discussionforums.js
+++ routes/api/discussionforums.js
@@ -12,13 +12,13 @@ router.post("/", async (req, res) => {
   //auth to be added
   try {
     const { title, description, user_id } = req.body;
-    const discussionForum = new Discussionforum({
+    const discussionforum = new Discussionforum({
       user_id,
       title,
       description,
     });
-    await discussionForum.save();
-    res.send(discussionForum);
+    await discussionforum.save();
+    res.send(discussionforum);
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
@@ -28,8 +28,8 @@ router.post("/", async (req, res) => {
 // GET all discussion forum topics
 router.get("/", async (req, res) => {
   try {
-    const discussionForums = await Discussionforum.find().populate("user_id");
-    res.send(discussionForums);
+    const discussionforums = await Discussionforum.find().populate("user_id");
+    res.send(discussionforums);
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
@@ -39,13 +39,13 @@ router.get("/", async (req, res) => {
 // GET a specific discussion forum topic by ID
 router.get("/:id", async (req, res) => {
   try {
-    const discussionForum = await Discussionforum.findById(
+    const discussionforum = await Discussionforum.findById(
       req.params.id
     ).populate("user_id");
-    if (!discussionForum) {
+    if (!discussionforum) {
       return res.status(404).send("Discussion forum not found");
     }
-    res.send(discussionForum);
+    res.send(discussionforum);
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
@@ -56,13 +56,13 @@ router.get("/:id", async (req, res) => {
 router.post("/:id/comments", async (req, res) => {
   try {
     const { user_id, content } = req.body;
-    const discussionForum = await Discussionforum.findById(req.params.id);
-    if (!discussionForum) {
+    const discussionforum = await Discussionforum.findById(req.params.id);
+    if (!discussionforum) {
       return res.status(404).send("Discussion forum not found");
     }
-    discussionForum.comments.push({ user_id, content });
-    await discussionForum.save();
-    res.status(201).send(discussionForum.comments);
+    discussionforum.comments.push({ user_id, content });
+    await discussionforum.save();
+    res.status(201).send(discussionforum.comments);
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
@@ -72,11 +72,11 @@ router.post("/:id/comments", async (req, res) => {
 // Delete a discussion forum topic
 router.delete("/:id", async (req, res) => {
   try {
-    const discussionForum = await Discussionforum.findById(req.params.id);
-    if (!discussionForum) {
+    const discussionforum = await Discussionforum.findById(req.params.id);
+    if (!discussionforum) {
       return res.status(404).send("Discussion forum not found");
     }
-    await discussionForum.remove();
+    await discussionforum.remove();
     res.status(200).send("Discussion forum post has been deleted");
   } catch (error) {
     console.error(error);
